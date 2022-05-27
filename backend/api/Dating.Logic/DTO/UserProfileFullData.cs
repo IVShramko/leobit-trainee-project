98481c5786f839a248eaac9ff75a908695119774
LEOBIT-ISHRAMKO\Ivan <ishramko@leobit.com>
2022-05-27 13:03:57 +0300

    created DTO

---
+++ backend/api/Dating.Logic/DTO/UserProfileFullData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dating.Logic.DTO
+{
+    public class UserProfileFullData
+    {
+        public Guid Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public bool Gender { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Region { get; set; }
+
+        public string Town { get; set; }
+    }
+}
+
---
+++ backend/api/Dating.Logic/DTO/UserProfileMainData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dating.Logic.DTO
+{
+    public class UserProfileMainData
+    {
+        public Guid Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public bool Gender { get; set; }
+
+        public string Email { get; set; }
+    }
+}
