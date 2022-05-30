0431bb65b84368c7e1de350ce887acca20c79b13
LEOBIT-ISHRAMKO\Ivan <ishramko@leobit.com>
2022-05-30 14:19:30 +0300

    changed userProfile model + migration

---
+++ backend/api/Dating.Logic/Migrations/20220530111722_ChangedUserProfile.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dating.Logic.Migrations
+{
+    public partial class ChangedUserProfile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "UserProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "UserProfiles");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "UserProfiles",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "UserProfiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- backend/api/Dating.Logic/Models/UserProfile.cs
+++ backend/api/Dating.Logic/Models/UserProfile.cs
@@ -1,17 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dating.Logic.Models
 {
     public class UserProfile
     {
-        [Key]
         public Guid Id { get; set; }
 
         [Required]
@@ -20,22 +14,13 @@ namespace Dating.Logic.Models
 
         public IdentityUser AspNetUser { get; set; }
 
-        [Required]
-        public string UserName { get; set; }
-
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
 
-        [Required]
         public DateTime BirthDate { get; set; }
 
-        [Required]
         public bool Gender { get; set; }
-
-        [Required]
-        [MaxLength(256)]
-        public string Email { get; set; }
 
         public string PhoneNumber { get; set; }
 
