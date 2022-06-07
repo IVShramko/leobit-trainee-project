1cacc906fe932db6d3a81df237ee76fdb11913fb
LEOBIT-ISHRAMKO\Ivan <ishramko@leobit.com>
2022-06-07 10:48:19 +0300

    made controller + DTO

---
+++ backend/api/Dating.Logic/DTO/CriteriaDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Dating.Logic.DTO
+{
+    public class CriteriaDTO
+    {
+        public bool Gender { get; set; }
+
+        public int MinAge { get; set; }
+
+        public int MaxAge { get; set; }
+
+        public string Region { get; set; }
+
+        public string Town { get; set; }
+    }
+}
---
+++ backend/api/api/Controllers/SearchController.cs
@@ -0,0 +1,16 @@
+﻿using Dating.Logic.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dating.WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Criteria(CriteriaDTO criteria)
+        {
+            return Ok();
+        }
+    }
+}
